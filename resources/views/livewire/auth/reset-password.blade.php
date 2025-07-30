1a9298e0c64903504b31521525cb5c93345f5bc6
Jesus Camargo <jebus_perfidia@outlook.com>
2025-07-30 13:24:50 -0600

    Correción de textos en recuperación de contraseña

--- resources/views/livewire/auth/reset-password.blade.php
+++ resources/views/livewire/auth/reset-password.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Reset password')" :description="__('Please enter your new password below')" />
+    <x-auth-header :title="__('Restaurar password')" :description="__('Por favor, ingrese un nuevo password')" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
@@ -28,17 +28,17 @@
         <!-- Confirm Password -->
         <flux:input
             wire:model="password_confirmation"
-            :label="__('Confirm password')"
+            :label="__('Confirmar password')"
             type="password"
             required
             autocomplete="new-password"
-            :placeholder="__('Confirm password')"
+            :placeholder="__('Confirmar password')"
             viewable
         />
 
         <div class="flex items-center justify-end">
             <flux:button type="submit" variant="primary" class="w-full">
-                {{ __('Reset password') }}
+                {{ __('Restaurar password') }}
             </flux:button>
         </div>
     </form>
